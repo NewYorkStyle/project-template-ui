79578255fd7dab1b8cafbfd3d58494a40c816a34
NewYorkStyle <39958500+NewYorkStyle@users.noreply.github.com>
2026-03-21 21:20:23 +0300

    feat: PROJECTTEMPLATE-55 Добавил локализацию (#6)

--- src/theme/ui-provider.tsx
+++ src/theme/ui-provider.tsx
@@ -1,6 +1,8 @@
 import {type ReactNode, useMemo} from 'react';
 
 import {ConfigProvider} from 'antd';
+import en_US from 'antd/locale/en_US';
+import ru_RU from 'antd/locale/ru_RU';
 
 import {designTokens, type TColorsPalette} from './design-tokens';
 import {getAntdThemeConfig} from './get-antd-tokens';
@@ -8,6 +10,7 @@ import {getAntdThemeConfig} from './get-antd-tokens';
 export type TUiProviderProps = {
   children: ReactNode;
   tokens?: Partial<TColorsPalette>;
+  language?: 'ru' | 'en';
 };
 
 const mergeColorsPalette = (
@@ -23,13 +26,32 @@ const mergeColorsPalette = (
   };
 };
 
-export const UiProvider = ({children, tokens}: TUiProviderProps) => {
+export const UiProvider = ({children, language, tokens}: TUiProviderProps) => {
   const mergedPalette = useMemo(() => mergeColorsPalette(tokens), [tokens]);
 
   const antdTheme = useMemo(
     () => getAntdThemeConfig(mergedPalette),
     [mergedPalette]
   );
+  const antLocale = useMemo(() => {
+    let antdLocale = en_US;
 
-  return <ConfigProvider theme={antdTheme}>{children}</ConfigProvider>;
+    switch (language) {
+      case 'ru':
+        antdLocale = ru_RU;
+        break;
+      case 'en':
+      default:
+        antdLocale = en_US;
+        break;
+    }
+
+    return antdLocale;
+  }, [language]);
+
+  return (
+    <ConfigProvider theme={antdTheme} locale={antLocale}>
+      {children}
+    </ConfigProvider>
+  );
 };
